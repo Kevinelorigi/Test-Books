916824f85649ff14059ec9965c15354f4e2d75c8
Kevinelorigi <kevinelorigi2002@gmail.com>
2024-02-18 11:58:41 -0500

    Finish

--- client/test-books/src/pages/Add.jsx
+++ client/test-books/src/pages/Add.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Add = () => {
-  return (
-    <div>
-      Add
-    </div>
-  )
-}
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  });
 
-export default Add
+  const [error, setError] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8800/books", book);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+      setError(true);
+    }
+  };
+
+  return (
+    <div className="form">
+      <h1>Agregar Nuevo Libro</h1>
+      <input
+        type="text"
+        placeholder="Titulo"
+        onChange={handleChange}
+        name="title"
+      />
+      <textarea
+        rows={5}
+        type="text"
+        placeholder="Descripción"
+        onChange={handleChange}
+        name="desc"
+      />
+      <input
+        type="number"
+        placeholder="Precio"
+        onChange={handleChange}
+        name="price"
+      />
+      <input
+        type="text"
+        placeholder="Imagen"
+        onChange={handleChange}
+        name="cover"
+      />
+      <button onClick={handleClick}>Agregar</button>
+    </div>
+  );
+};
+
+export default Add;
