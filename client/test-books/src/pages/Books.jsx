65c99469b0cade7a97d71986a5dcf8808d521269
Kevinelorigi <kevinelorigi2002@gmail.com>
2024-02-18 11:45:11 -0500

    Add new

--- client/test-books/src/pages/Books.jsx
+++ client/test-books/src/pages/Books.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from "react-router-dom";
 import axios from "axios"
 
 const Books = () => {
 
-  const [book, setBooks] = useState()
+  const [books, setBooks] = useState([]);
 
   useEffect(() =>{
     const fecthAllBooks = async ()=>{
       try{
         const res = await axios.get("http://localhost:8800/books")
-        console.log(res)
+        setBooks(res.data)
       }catch(err){
         console.log(err)
       }
@@ -19,7 +20,18 @@ const Books = () => {
 
   return (
     <div>
-      Book
+      <h1>Libreria</h1>
+      <div className='books'>
+        {books.map(book=>(
+          <div key={book.id} className='book'>
+            {book.cover && <img src={book.cover} alt=''/>}
+            <h2>{book.title}</h2>
+            <h2>{book.desc}</h2>
+            <span>{book.price}</span>
+          </div>
+        ))}
+      </div>
+      <button><Link to="/add">Agregar nuevo libro</Link></button>
     </div>
   )
 }
